12f90192b1bd5db8eec76a91197e158af686236b
Afonso Kevin Burginski <83406890+afonsoburginski@users.noreply.github.com>
2024-04-10 11:33:56 -0400

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPeAiXpaNmtMa1cisK4gXqvdlRvEKphzs",
-  authDomain: "vortex-plus-b06f7.firebaseapp.com",
-  projectId: "vortex-plus-b06f7",
-  storageBucket: "vortex-plus-b06f7.appspot.com",
-  messagingSenderId: "690193069668",
-  appId: "1:690193069668:web:8fb20f9651df91628d1171",
-  measurementId: "G-5ENQQLXGXG"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
